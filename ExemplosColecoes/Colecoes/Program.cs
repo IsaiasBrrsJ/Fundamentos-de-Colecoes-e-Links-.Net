4ba57550b7f5518cf966f87d8f9e3f7e1715ed39
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2023-01-04 10:11:23 -0400

    Arrays
    Array unidimensional

--- ExemplosColecoes/Colecoes/Program.cs
+++ ExemplosColecoes/Colecoes/Program.cs
@@ -6,6 +6,22 @@ namespace Colecoes
     {
         static void Main(string[] args)
         {
+            int[] arrayInteiros = new int[3];
+            //Começa em 0 e vai até (n -1);
+            arrayInteiros[0] = 10;
+            arrayInteiros[1] = 20;
+            arrayInteiros[2] = 30;
+
+            Console.WriteLine("Percorrendo array pelo For");
+            for (int i = 0; i < arrayInteiros.Length; i++)
+            {
+                Console.WriteLine(arrayInteiros[i]);
+            }
+            Console.WriteLine("Percorrendo array pelo ForEach");
+            foreach (var item in arrayInteiros)
+            {
+                Console.WriteLine(item);
+            }
 
         }
     }
