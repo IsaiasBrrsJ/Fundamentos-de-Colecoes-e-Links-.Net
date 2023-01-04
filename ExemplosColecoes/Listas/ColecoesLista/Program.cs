0dd7993a9cfda4b2673f9995730a5dad9185b9a5
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2023-01-04 19:20:27 -0400

    Listas
    Estudando listas

---
+++ ExemplosColecoes/Listas/ColecoesLista/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+List<int> valores = new List<int> { 3, 4, 5, 6, 3 };
+
+//adcionar um novo valor em uma posição
+valores.Insert(1, 10);
+
+
+valores.Remove(3); // remove o primeiro elemento que encontrar com esse valor que aqui no caso é 3
+var exibirValore = string.Join(",", valores); // exibe os valores da lista separados por ","
+Console.WriteLine(exibirValore);
