d7fcc7d6de54c95e5c63e95ce0f4f818d9b9ca6f
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2023-01-05 16:48:25 -0400

    Dicionarios
    Praticando com dicionarios KeyValuePair

---
+++ ExemplosColecoes/Dicionarios/Program.cs
@@ -0,0 +1,37 @@
+﻿Dictionary<string, string> estados = new Dictionary<string, string>();
+
+
+estados.Add("SP", "São Paulo");
+estados.Add("MG", "Minas Gerais");
+estados.Add("BA", "Bahia");
+
+
+foreach (KeyValuePair<string, string> item in estados)
+{
+    Console.WriteLine("Chave: " + item.Key + " Valor: " + item.Value);
+}
+
+var valorBuscado = "BA";
+// Console.WriteLine("Buscando pela chave");
+// Console.WriteLine(estados[valorBuscado]);
+
+//Atualizando
+Console.WriteLine("Valor");
+Console.WriteLine(estados[valorBuscado]);
+estados[valorBuscado] = "BA - Valor Atualizado";
+Console.WriteLine("Valor Atualizado");
+Console.WriteLine(estados[valorBuscado]);
+
+Console.WriteLine("Remover um valor");
+estados.Remove(valorBuscado);
+
+//Acessar de maneira segura
+
+if (estados.TryGetValue(valorBuscado, out string valorEncontrado)) // tenta buscar o valor 
+{
+    Console.WriteLine("Valor encontrado\n " + valorEncontrado);
+}
+else
+{
+    Console.WriteLine("Valor não encontrado");
+}
